7ebf773c7d6789db2856abc0c780a5a1f1344c32
jimosans <jimosans@gmail.com>
2023-11-05 17:22:27 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+defineProps({
+    blogs: Array
+})
+</script>
+<template>
+    <div v-if="$page.props.flash.message" class="bg-red-500 alert alert-success">
+        {{ $page.props.flash.message }}
+    </div>
+    <ul>
+        <li v-for="blog in blogs" :key="blog.id">
+            <Link class="text-blue-500" :href="route('inertia.show', {id: blog.id})">件名:{{ blog.title }},本文:{{ blog.content }}</Link>
+        </li>
+    </ul>
+あああああ
+</template>
---
+++ resources/js/Pages/Inertia/show.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia'
+
+defineProps({
+    id: String, // ここで型を指定する
+    blog: Object
+})
+
+const deleteConfirm = id => {
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('削除しますか？')
+    })
+}
+</script>
+
+<template>
+    <h1>id: {{ id }}</h1>
+    {{ blog.title }}
+    {{ blog.content }}
+    <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
+
